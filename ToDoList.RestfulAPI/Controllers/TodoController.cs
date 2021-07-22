db7f45aa032ba92b45ce56792c4ffca246e55d78
baroniunas91 <61449435+baroniunas91@users.noreply.github.com>
2021-07-22 17:35:59 +0300

    Create TodoController and Todo Model

---
+++ ToDoList.RestfulAPI/Controllers/TodoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using ToDoList.RestfulAPI.Models;
+
+namespace ToDoList.RestfulAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ToDoController : ControllerBase
+    {
+        
+    }
+}
---
+++ ToDoList.RestfulAPI/Models/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.RestfulAPI.Models
+{
+    public class Todo
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
